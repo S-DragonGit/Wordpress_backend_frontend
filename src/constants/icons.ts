da20e09eff656106d81d73a68fe53df0e0b717ba
S-DragonGit <SHomeyParadise228@gmail.com>
2025-01-08 23:45:23 +0530

    change geo icon

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -3,7 +3,7 @@ import Table from '../assets/icons/table.svg';
 import Calendar from '../assets/icons/calendar.svg';
 import PlayOnce from '../assets/icons/play-once.svg';
 import CalendarTwo from '../assets/icons/calendarTwo.svg';
-import GeoIcon from '../assets/icons/Geo.svg';
+import GeoIcon from '../assets/icons/geo-icon.svg';
 
 const icons = {
     Table,
